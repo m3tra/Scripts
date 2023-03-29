14767e677a785d687acf5943261a3a293244bcdb
m3tra <m3tra1337@gmail.com>
2023-03-29 05:19:44 +0100

    Update vsftpd.sh

--- vsftpd.sh
+++ vsftpd.sh
@@ -18,11 +18,11 @@ service vsftpd status
 
 ## UFW RUles
 
-echo " \
-[VSFTPD] \
-title=Secure FTP Daemon \
-description=TLS encrypted FTP server \
-ports=20,21,990/tcp|40000:50000/tcp \
+echo "
+[VSFTPD]
+title=Secure FTP Daemon
+description=TLS encrypted FTP server
+ports=20,21,990/tcp|40000:50000/tcp
 " > /etc/ufw/applications.d/vsftpd
 
 ufw allow from 192.168.0.0/16 to any app vsftpd comment VSFTPD
@@ -43,31 +43,32 @@ chown $USER:$USER ~/ftp/files
 
 mv /etc/vsftpd.conf /etc/vsftpd.conf.bak
 
-echo "\
-listen=NO \
-listen_ipv6=NO \
-anonymous_enable=NO \
-local_enable=YES \
-write_enable=YES \
-local_umask=022 \
-dirmessage_enable=YES \
-use_localtime=YES \
-xferlog_enable=YES \
-connect_from_port_20=YES \
-chroot_local_user=YES \
-secure_chroot_dir=/var/run/vsftpd/empty \
-pam_service_name=vsftpd \
-force_dot_files=YES \
-\
-pasv_min_port=40000 \
-pasv_max_port=50000 \
-\
-user_sub_token=$USER \
-local_root=/home/$USER/ftp \
-\
-userlist_enable=YES \
-userlist_file=/etc/vsftpd.userlist \
-userlist_deny=NO" > /etc/vsftpd.conf
+echo "
+listen=NO
+listen_ipv6=NO
+anonymous_enable=NO
+local_enable=YES
+write_enable=YES
+local_umask=022
+dirmessage_enable=YES
+use_localtime=YES
+xferlog_enable=YES
+connect_from_port_20=YES
+chroot_local_user=YES
+secure_chroot_dir=/var/run/vsftpd/empty
+pam_service_name=vsftpd
+force_dot_files=YES
+
+pasv_min_port=40000
+pasv_max_port=50000
+
+user_sub_token=$USER
+local_root=/home/$USER/ftp
+
+userlist_enable=YES
+userlist_file=/etc/vsftpd.userlist
+userlist_deny=NO
+" > /etc/vsftpd.conf
 
 
 echo $USER > /etc/vsftpd.userlist
@@ -85,18 +86,18 @@ openssl req -x509 -nodes -days 3650 -newkey rsa:4096 -keyout /etc/ssl/private/vs
 
 ## Configure TLS
 
-echo "\
-\
-ssl_enable=YES \
-rsa_cert_file=/etc/ssl/private/vsftpd.pem \
-rsa_private_key_file=/etc/ssl/private/vsftpd.pem \
-allow_anon_ssl=NO \
-force_local_data_ssl=YES \
-force_local_logins_ssl=YES \
-ssl_tlsv1=YES \
-ssl_sslv2=NO \
-ssl_sslv3=NO \
-require_ssl_reuse=NO \
-ssl_ciphers=HIGH" >> /etc/vsftpd.conf
+echo "
+ssl_enable=YES
+rsa_cert_file=/etc/ssl/private/vsftpd.pem
+rsa_private_key_file=/etc/ssl/private/vsftpd.pem
+allow_anon_ssl=NO
+force_local_data_ssl=YES
+force_local_logins_ssl=YES
+ssl_tlsv1=YES
+ssl_sslv2=NO
+ssl_sslv3=NO
+require_ssl_reuse=NO
+ssl_ciphers=HIGH
+" >> /etc/vsftpd.conf
 
 systemctl restart vsftpd.service
