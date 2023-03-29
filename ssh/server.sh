1af80d5abfdf7e5905d4e49aa21aac5cc26732cd
m3tra <m3tra1337@gmail.com>
2023-03-29 04:58:18 +0100

    Update server.sh

--- ssh/server.sh
+++ ssh/server.sh
@@ -107,7 +107,7 @@ elif [[ $(cat /etc/os-release | grep -i "Ubuntu" | wc -l) -ne 0 ]]; then
 			Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
 			MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com
 			HostKeyAlgorithms ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,sk-ssh-ed25519@openssh.com,sk-ssh-ed25519-cert-v01@openssh.com,rsa-sha2-512,rsa-sha2-512-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-256-cert-v01@openssh.com" > /etc/ssh/sshd_config.d/ssh-audit_hardening.conf
-	if [[ $(cat /etc/os-release | grep -i "Ubuntu 22.04" | wc -l) -ne 0 ]]; then
+	elif [[ $(cat /etc/os-release | grep -i "Ubuntu 22.04" | wc -l) -ne 0 ]]; then
 		echo -e "
 			# Restrict key exchange, cipher, and MAC algorithms, as per sshaudit.com
 			# hardening guide.
@@ -123,3 +123,5 @@ service ssh restart
 ## Test
 
 sshd -T
+
+sudo ufw limit $PORT/tcp
