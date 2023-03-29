396cc187a8cdea514ff81c218e5fcf09d438e8b5
m3tra <m3tra1337@gmail.com>
2023-03-29 04:14:19 +0100

    Fixed Ubuntu/Debian detection

--- ssh/client.sh
+++ ssh/client.sh
@@ -7,8 +7,8 @@ fi
 
 mkdir -p -m 0700 ~/.ssh
 
-if [[ $(cat /etc/os-release | grep "Ubuntu 22.04" | wc -l) -ne 0 ]]; then
+if [[ $(cat /etc/os-release | grep -i "Ubuntu 22.04" | wc -l) -ne 0 ]]; then
 	echo -e "\nHost *\n Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr\n KexAlgorithms sntrup761x25519-sha512@openssh.com,gss-curve25519-sha256-,curve25519-sha256,curve25519-sha256@libssh.org,gss-group16-sha512-,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256\n MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com\n HostKeyAlgorithms ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,sk-ssh-ed25519@openssh.com,sk-ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-512,rsa-sha2-512-cert-v01@openssh.com\n" >> ~/.ssh/config
-elif if [[ $(cat /etc/os-release | grep "Ubuntu 20.04" | wc -l) -ne 0 ]]; then
+elif if [[ $(cat /etc/os-release | grep -i "Ubuntu 20.04" | wc -l) -ne 0 ]]; then
 	echo -e "\nHost *\n Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr\n  KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256\n  MACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com\n  HostKeyAlgorithms ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,sk-ssh-ed25519@openssh.com,sk-ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-512,rsa-sha2-512-cert-v01@openssh.com\n" >> ~/.ssh/config
 fi
